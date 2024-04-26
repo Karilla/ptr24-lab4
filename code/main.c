3c2f8ea4f342fb930b150e3fc4d42a33330a932e
Karilla <benoit.delay@heig-vd.ch>
2024-04-26 15:24:27 +0200

    It workd better now

--- code/main.c
+++ code/main.c
@@ -9,21 +9,34 @@
 #include <alchemy/timer.h>
 #include "audio_utils.h"
 
+#define PERIOD_AUDIO 1333333
+
+#define DEBUG 1
+
+typedef void *audio_data_t;
+
 void audio_task(void *arg)
 {
-	struct *wav_header wh;
-    struct wav_file wf;
+	ssize_t nb_byte_read;
+	rt_task_set_periodic(rt_task_self(), TM_NOW, PERIOD_AUDIO);
+	while (1)
+	{
+		nb_byte_read = read_samples(arg, 256);
+		write_samples(arg, nb_byte_read);
+		if (rt_task_wait_period(NULL))
+		{
+			rt_printf("Delai depassed\n");
+		}
+	}
+}
 
-    uint16_t *audio_data; 
-
-
-
-
-    rt_task_wait_period(NULL);
+void video_task(void *arg)
+{
 }
 
 int main(int argc, char *argv[])
 {
+	uint16_t *audio_data;
 	init_audio();
 
 	init_ioctl(0);
@@ -33,24 +46,33 @@ int main(int argc, char *argv[])
 
 	int ret;
 
+	audio_data = (audio_data_t *)calloc(256, sizeof(audio_data_t));
+	if (audio_data == NULL)
+	{
+		rt_printf("Error allocating memory\n");
+		exit(EXIT_FAILURE);
+	}
 	mlockall(MCL_CURRENT | MCL_FUTURE);
 
-	if (rt_task_create(&timer, "timer", 0, 99, T_JOINABLE) != 0) {
+	if (rt_task_create(&audio, "timer", 0, 99, T_JOINABLE) != 0)
+	{
 		rt_printf("Error creating task\n");
 		exit(EXIT_FAILURE);
 	}
 
-	if (rt_task_start(&timer, &xenomai_timer, NULL) != 0) {
+	if (rt_task_start(&audio, &audio_task, audio_data) != 0)
+	{
 		rt_printf("Error starting task\n");
 		exit(EXIT_FAILURE);
 	}
 
-	if (rt_task_join(&timer) != 0) {
+	if (rt_task_join(&audio) != 0)
+	{
 		rt_printf("Error joining task\n");
 		exit(EXIT_FAILURE);
 	}
 
-	rt_task_delete(&timer);
+	rt_task_delete(&audio);
 
 	munlockall();
 
